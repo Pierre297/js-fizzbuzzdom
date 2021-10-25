aee65231ec4f24e16e159e69956c42b033a85df0
Pier <pierfrancesco297@gmail.com>
2021-10-25 15:07:01 +0200

    loop FizzBUzz

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,37 @@
-const squareElement =  document.getElementById("containerSquare");
+const squareElement =  document.getElementById("container-square");
 
 for( let i = 1; i <= 100; i++){
     
+    // prima condizione Fizz
+    if(i % 3 == 0){
+        squareElement.innerHTML += `<div class="square fizz">fizz</div>`;
+    }
+    // seconda condizione buzz
+    else if(i % 5 == 0){
+        squareElement.innerHTML += `<div class="square buzz">buzz</div>`;
+    }
+    // terza condizione FizzBuzz
+    else if(i % 3 == 0 && i % 5 == 0){
+        squareElement.innerHTML += `<div class="square fizzbuzz">FizzBuzz</div>`;
+    }
+    // ultima condizione
+    else{
+    squareElement.innerHTML += `<div class="square">
+    ${i} </div>`;
 }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
